4fdbc32e65f6b95b004be0dbe42ee7d1cd978d40
EC2 Default User <ec2-user@ip-172-31-77-116.ec2.internal>
2021-01-31 20:33:43 +0000

    language

--- todos/get.py
+++ todos/get.py
@@ -6,7 +6,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 client = boto3.client('comprehend')
-
+clientTranslate = boto3.client('translate')
 
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
@@ -36,20 +36,31 @@ def get_todo(event, context):
             'id': event['pathParameters']['id']
         }
     )
-
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(result['Item'],
-                           cls=decimalencoder.DecimalEncoder)
-    }
-    
-    language_code = event['pathParameters']['language']
-    
     try:
-        response = client.detect_syntax(
-                Text=response, LanguageCode=language_code)
+       objeto = json.dumps(result['Item'],
+                           cls=decimalencoder.DecimalEncoder)
+       print('1')     
+       language_code = event['pathParameters']['language']
+       print('2')     
+       translate = client.detect_dominant_language(Text=result['Item']['text'])
+       print('3')     
+       print(json.dumps(translate))
+       language_code_init = translate['Languages'][0]['LanguageCode']
+       print('4')     
+       print('language_code= ' + language_code)
+       print('language_code_init= ' + language_code_init)
+       print('5')     
+       textTranslate = clientTranslate.translate_text(Text=result['Item']['text'], SourceLanguageCode=language_code_init, TargetLanguageCode=language_code)
+       print('6')     
+       # create a response
+   
+       
     except Exception:
             raise
+
+    response = {
+          "statusCode": 200,
+          "body": ''
+    }
 
     return response

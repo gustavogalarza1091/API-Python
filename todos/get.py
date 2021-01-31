0592de17c5be62aa00ed1abba7f19fccf53903c1
EC2 Default User <ec2-user@ip-172-31-77-116.ec2.internal>
2021-01-31 19:20:02 +0000

    comprenhead

--- todos/get.py
+++ todos/get.py
@@ -5,6 +5,8 @@ from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
+client = boto3.client('comprehend')
+
 
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
@@ -22,5 +24,32 @@ def get(event, context):
         "body": json.dumps(result['Item'],
                            cls=decimalencoder.DecimalEncoder)
     }
+
+    return response
+
+def get_todo(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Item'],
+                           cls=decimalencoder.DecimalEncoder)
+    }
+    
+    language_code = event['pathParameters']['language']
+    
+    try:
+        response = client.detect_syntax(
+                Text=response, LanguageCode=language_code)
+    except Exception:
+            raise
 
     return response
